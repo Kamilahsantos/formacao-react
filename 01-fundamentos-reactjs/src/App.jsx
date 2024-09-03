171e4e2be5e5da5e1007d2edc72b322c093e37b3
Kamila Santos <kahsantos89@gmail.com>
2024-09-02 23:40:34 -0300

    aula 1 - fundamentos

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,20 @@
+import {Post} from './Post';
+export function App() {
+  return (
+    <div>
+   <Post 
+      author="Kamila Santos"
+      content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate saepe earum similique in nemo cumque accusantium hic. Magni aliquam ipsum voluptatibus temporibus quam dicta dolor quasi et, expedita eos voluptatum!"
+    />
+   <Post 
+   author="teste"
+   content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate saepe earum similique in nemo cumque accusantium hic. Magni aliquam ipsum voluptatibus temporibus quam dicta dolor quasi et, expedita eos voluptatum!"
+   />
+   <Post/>
+   <Post/>
+   <Post/>
+   </div>
+  )
+}
+
+
